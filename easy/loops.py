a973be871d32bf07b697eabadd4e7943280cddd8
nolifer_jpg <roysouhard2706@gmail.com>
2025-05-19 01:12:46 +0530

    Done solve_me_first

--- easy/loops.py
+++ easy/loops.py
@@ -1,1 +1,1 @@
-# TODO: Loops
+#todo
--- easy/solve_me_first.py
+++ easy/solve_me_first.py
@@ -1,1 +1,12 @@
-# TODO: Solve Me First
+def solveMeFirst(a,b):
+    if 1 <= a <= 1000 and 1 <= b <= 1000:
+        return a+b
+    else:
+        return "Give a valid number"
+	
+
+
+num1 = int(input("Enter a first number: "))
+num2 = int(input("Enter second number: "))
+res = solveMeFirst(num1,num2)
+print(res)
