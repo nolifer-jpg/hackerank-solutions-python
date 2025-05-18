d62b499c6fbf1c1a2d64555b2d093dd656b606d6
nolifer_jpg <roysouhard2706@gmail.com>
2025-05-19 01:27:29 +0530

    done simple array sum

--- easy/simple_array_sum.py
+++ easy/simple_array_sum.py
@@ -1,1 +1,42 @@
-# TODO: Simple Array Sum
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(arr):
+    total = 0
+    for i in arr:
+        total += i
+    return total    
+
+
+# Input
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Call and print result
+print(simpleArraySum(arr))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
